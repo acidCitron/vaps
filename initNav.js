9cb683e0abe354efd9fc7d43556da90f054ba722
acidCitron <102357124+acidCitron@users.noreply.github.com>
2022-03-25 11:04:39 +0100

    Add files via upload

---
+++ initNav.js
@@ -0,0 +1,5 @@
+"use strict";
+	const listElem = document.getElementById('brandsVapotech');
+	const brandList = new AlphaListNav(listElem, {initLetter: 'a',});
+	const listElem2 = document.getElementById('brandsAromes');
+	const brandList2 = new AlphaListNav(listElem2, {initLetter: 'a',});
